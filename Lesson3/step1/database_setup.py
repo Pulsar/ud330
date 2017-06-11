2a604b8f703e0c9d90a99ab20ec34efb8cfbd00e
Pulsar <gjergjlala@gmx.de>
2017-06-11 22:16:38 +0200

    Authroisation rules
    Create authroisation rules for users.

--- Lesson3/step1/database_setup.py
+++ Lesson3/step1/database_setup.py
@@ -9,10 +9,10 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    picture = Column(String(250))
+    id      = Column(Integer    , primary_key =True )
+    name    = Column(String(250), nullable    =False)
+    email   = Column(String(250), nullable    =False)
+    picture = Column(String(250)                    )
 
 
 class Restaurant(Base):
